a1cffda3c11e38d63fc4713cd763ca3f2e6a1a41
Serinajefthas <95409426+Serinajefthas@users.noreply.github.com>
2023-05-25 02:35:34 +0200

    Update main.h

--- main.h
+++ main.h
@@ -7,4 +7,4 @@
 void cmd_exe(char **argv);
 char *get_path(char *command);
 void main_loop(char *cmd, char *cmd_cp, char *argv[]);
-int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
